e49a32d9e0d2a388067db18be0a7f2cf15d0141b
Pramudhi Perera <60996450+phperera@users.noreply.github.com>
2022-09-05 19:26:06 +0530

    Update Printer.java

--- threads/Printer.java
+++ threads/Printer.java
@@ -1,1 +1,32 @@
+public class Printer implements Runnable{
+    
+    
+ /*  public void start(){
+       // super.start();
+        System.out.println("this is start");
+        super.start();
+    }*/
+    
+    @Override
+    public void run(){
+       // System.out.println("his is from child thread");
+    for(int i=0;i<50;i++){
+        System.out.println("child "+Thread.currentThread().getName()+" "+ i);
+      try {
+            Thread.sleep(250);
+        } catch (InterruptedException ex) {
+            System.out.println("Im up...");
+            //Logger.getLogger(Printer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     
+    }
+       
+   
+    //can overload the run methods but always the no argumnet method will invoke first
+ //   public void run(int x){
+   //     for(int i=0;i<10;i++){
+      //  System.out.println("child "+Thread.currentThread().getName()+" "+i);
+    //}
+    }
+}
 
