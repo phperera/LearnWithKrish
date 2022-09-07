abeb32033d85bd856907892bd8449e0f0881645d
Pramudhi Perera <60996450+phperera@users.noreply.github.com>
2022-09-07 22:20:15 +0530

    Add files via upload

---
+++ Kafka/springboot-kafkka/src/main/java/net/javaguides/springbootkafkka/SpringbootKafkkaApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springbootkafkka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkkaApplication.class, args);
+	}
+
+}
